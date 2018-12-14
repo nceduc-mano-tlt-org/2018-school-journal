8d1ef52f3355375b6a3c2e12faabba0bb64ec6e2
Alexey <forhotos@gmail.com>
2018-12-14 22:17:36 +0300

    Fixed some issues
    1. Teacher JDBC Dao now implements Journal Dao
    2. Added Connector interface & ConnectorPostgre class

--- src/main/java/ru/nceduc/journal/dao/Connector.java
+++ src/main/java/ru/nceduc/journal/dao/Connector.java
@@ -1,21 +1,7 @@
 package ru.nceduc.journal.dao;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
-import java.sql.SQLException;
 
-public class Connector {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "user";
-    private static final String PASS = "user1";
-
-    public static Connection getConnection() {
-        Connection connection;
-        try {
-            return connection = DriverManager.getConnection(URL, USER, PASS);
-        } catch (SQLException ex) {
-            throw new RuntimeException("Error connecting to the database", ex);
-        }
-    }
+public interface Connector {
+    Connection getConnection(String url, String username, String password);
 }
-
